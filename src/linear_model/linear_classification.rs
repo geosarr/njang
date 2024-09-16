4a52b6e9a6ce493e8e396ce9c2fb1e5b788eecc6
Georges SARR <georgesmbissanes@gmail.com>
2024-09-16 21:57:26 +0200

    Added ridge classifier.

--- src/linear_model/linear_classification.rs
+++ src/linear_model/linear_classification.rs
@@ -1,3 +1,9 @@
+use num_traits::Float;
+
+use crate::traits::Container;
+
+use super::{LinearRegression, LinearRegressionSettings, LinearRegressionSolver};
+
 #[derive(Default, Debug, Clone, Copy)]
 pub enum LinearClassificationSolver {
     /// Uses Stochastic Gradient Descent
@@ -20,4 +26,59 @@ pub enum LinearClassificationSolver {
     /// The user should standardize the input predictors, otherwise the
     /// algorithm may not converge.
     Sag,
+    // ///
+    // Exact
+    // ///
+    // Svd
+    // ///
+    // Qr
+    // ///
+    // Cholesky
+}
+
+pub type RidgeClassificationSolver = LinearRegressionSolver;
+pub struct RidgeClassificationSettings<T> {
+    pub fit_intercept: bool,
+    pub solver: RidgeClassificationSolver,
+    pub l2_penalty: Option<T>,
+    pub tol: Option<T>,
+    pub step_size: Option<T>,
+    pub random_state: Option<u32>,
+    pub max_iter: Option<usize>,
+}
+
+pub struct RidgeClassification<C, I>
+where
+    C: Container,
+{
+    model: LinearRegression<C, I>,
+}
+
+impl<C: Container, I> RidgeClassification<C, I> {
+    pub fn new(settings: RidgeClassificationSettings<C::Elem>) -> Self
+    where
+        C::Elem: Float,
+    {
+        let lin_settings = LinearRegressionSettings {
+            fit_intercept: settings.fit_intercept,
+            solver: settings.solver,
+            l1_penalty: None,
+            l2_penalty: settings.l2_penalty,
+            tol: settings.tol,
+            step_size: settings.step_size,
+            random_state: settings.random_state,
+            max_iter: settings.max_iter,
+        };
+        Self {
+            model: LinearRegression::new(lin_settings),
+        }
+    }
+    /// Coefficients of the model
+    pub fn coef(&self) -> Option<&C> {
+        self.model.parameter.coef.as_ref()
+    }
+    /// Intercept of the model
+    pub fn intercept(&self) -> Option<&I> {
+        self.model.parameter.intercept.as_ref()
+    }
 }
