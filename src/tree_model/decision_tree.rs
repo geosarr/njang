78413c7dda5903428836084ffcc80617dbbb3115
Georges SARR <georgesmbissanes@gmail.com>
2024-11-16 18:22:38 +0100

    Added settings.

--- src/tree_model/decision_tree.rs
+++ src/tree_model/decision_tree.rs
@@ -6,4 +6,12 @@ pub struct DecisionNode<T> {
     pub right: Box<DecisionNode<T>>,
 }
 
-pub struct DecisionTreeSettings<T> {}
+pub struct DecisionTreeSettings<T> {
+    pub min_samples_split: T,
+    pub min_impurity: T,
+    pub max_depth: T,
+    pub impurity_calculation: T,
+    pub leaf_value_calculation: T,
+    pub one_dim: T,
+    // pub loss: T,
+}
